8aec4a51f126c3106d89d11d94b53435231b9014
Izel Ozhan <izelozhan@gmail.com>
2025-09-26 16:22:32 -0500

    initial

---
+++ Budgeteer/Budgeteer/BudgeteerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgeteerApp.swift
+//  Budgeteer
+//
+//  Created by Izel on 9/26/25.
+//
+
+import SwiftUI
+
+@main
+struct BudgeteerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Budgeteer/Budgeteer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Budgeteer
+//
+//  Created by Izel on 9/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
